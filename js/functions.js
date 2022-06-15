4fb5da8f1cf35040a967bc9eed137c6173f62d3b
alrodriguezdg <alrodriguezdg@gmail.com>
2022-06-15 10:18:02 -0500

    Ajustes dayro

--- js/functions.js
+++ js/functions.js
@@ -1,40 +1,4 @@
-$(function () {
-  const $bl = $(".thumbs-block"),
-    $th = $(".thumbs"),
-    blW = $bl.outerWidth(),
-    blSW = $bl.prop("scrollWidth"),
-    wDiff = blSW / blW - 1, // widths difference ratio
-    mPadd = 60, // Mousemove Padding
-    damp = 50; // Mousemove response softness
-
-  let posX = 0,
-    mX2 = 0, // Modified mouse position
-    mmAA = blW - mPadd * 2, // The mousemove available area
-    mmAAr = blW / mmAA, // get available mousemove fidderence ratio
-    itv = null;
-
-  const anim = () => {
-    posX += (mX2 - posX) / damp; // zeno's paradox equation "catching delay"
-    $th.css({
-      transform: `translateX(${-posX * wDiff}px)`,
-    });
-  };
-
-  $bl
-    .on("mousemove", function (e) {
-      const mouseX = e.pageX - $(this).prop("offsetLeft");
-      mX2 = Math.min(Math.max(0, mouseX - mPadd), mmAA) * mmAAr;
-    })
-    .on("mouseenter", function (e) {
-      itv = setInterval(anim, 10);
-    })
-    .on("mouseleave", function () {
-      clearInterval(itv);
-    });
-});
-
-gsap.registerPlugin(ScrollTrigger);
-gsap.registerPlugin(Draggable);
+gsap.registerPlugin(ScrollTrigger, Draggable);
 
 const imageContainer = document.querySelector(".zoom");
 imageContainer.onmousemove = (event) => {
@@ -59,11 +23,76 @@ for (var i = 0; i < thumbs.length; i++) {
   };
 }
 
-/** Gallery */
+/* toggle */
 
-$(".photo").on("click", function() {
-  $(".photo").toggleClass("away");
-  $(this)
-    .removeClass("away")
-    .toggleClass("active");
-});
+// Show an element
+var show = function (elem) {
+
+	// Get the natural height of the element
+	var getHeight = function () {
+		elem.style.display = 'block'; // Make it visible
+		var height = elem.scrollHeight + 'px'; // Get it's height
+		elem.style.display = ''; //  Hide it again
+		return height;
+	};
+
+	var height = getHeight(); // Get the natural height
+	elem.classList.add('is-visible'); // Make the element visible
+	elem.style.height = height; // Update the max-height
+
+	// Once the transition is complete, remove the inline max-height so the content can scale responsively
+	window.setTimeout(function () {
+		elem.style.height = '';
+	}, 350);
+
+};
+
+// Hide an element
+var hide = function (elem) {
+
+	// Give the element a height to change from
+	elem.style.height = elem.scrollHeight + 'px';
+
+	// Set the height back to 0
+	window.setTimeout(function () {
+		elem.style.height = '0';
+	}, 1);
+
+	// When the transition is complete, hide it
+	window.setTimeout(function () {
+		elem.classList.remove('is-visible');
+	}, 350);
+
+};
+
+// Toggle element visibility
+var toggle = function (elem, timing) {
+
+	// If the element is visible, hide it
+	if (elem.classList.contains('is-visible')) {
+		hide(elem);
+		return;
+	}
+
+	// Otherwise, show it
+	show(elem);
+	
+};
+
+// Listen for click events
+document.addEventListener('click', function (event) {
+
+	// Make sure clicked element is our toggle
+	if (!event.target.classList.contains('toggle')) return;
+
+	// Prevent default link behavior
+	event.preventDefault();
+
+	// Get the content
+	var content = document.querySelector(event.target.hash);
+	if (!content) return;
+
+	// Toggle the content
+	toggle(content);
+
+}, false);
