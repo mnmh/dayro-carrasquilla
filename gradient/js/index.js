c81a7bb7a17c7a18aecb23dd39efab934258c8e1
Crix <okcrix@gmail.com>
2022-06-22 19:47:56 -0500

    Animación degradado del background

--- gradient/js/index.js
+++ gradient/js/index.js
@@ -1,19 +1,36 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const paragraphs = Array.from(document.querySelectorAll("#container p"));
-paragraphs.shift(); 
-const titles = Array.from(document.querySelectorAll("#container h2"));
-const manos = Array.from(document.querySelectorAll(".manoBox"));
-//const gradient = document.querySelectorAll(".gradient");
+const container = document.querySelector("#container");
+const titles = Array.from(container.querySelectorAll("h2"));
+const paragraphs = Array.from(container.querySelectorAll("p"));
+paragraphs.shift();
+const manos = Array.from(container.querySelectorAll(".manoBox"));
+const gradient = document.querySelector("#gradient");
+const colors = ["rgb(252, 203, 104)", "rgb(252, 203, 104)", "rgb(203, 152, 102)", "rgb(203, 102, 51)", "rgb(101, 152, 102)", "rgb(102, 152, 51)", "rgb(51, 152, 152)", "rgb(52, 103, 103)"];
+const colorsTriggers = [];
+
+let divBox = document.createElement("div");
+divBox.id = "colorsBox";
+container.appendChild(divBox);
+
+gradient.style.background = `linear-gradient(to bottom, ${colors[0]}, ${colors[1]}`;
+
+for (let i = 0; i < (colors.length - 2); ++i){
+  let div = document.createElement("div");
+  div.classList.add("colorTrigger");
+  divBox.appendChild(div);
+  div.style.height = `${100/(colors.length - 2)}%`;
+  colorsTriggers.push(div);
+}
 
 const locoScroll = new LocomotiveScroll({
-  el: document.querySelector("#container"),
+  el: container,
   smooth: true,
   smoothMobile: true,
   scrollFromAnywhere: true,
 });
 
-ScrollTrigger.scrollerProxy("#container", {
+ScrollTrigger.scrollerProxy(container, {
   scrollTop(value) {
     return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
   },
@@ -21,57 +38,16 @@ ScrollTrigger.scrollerProxy("#container", {
     return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight,
     };
   },
-  pinType: document.querySelector("#container").style.transform ? "transform" : "fixed"
-});
-
-/* gsap.to("html", {
-  "--color1": "#98328e",
-  "--color2": "#98328e",
-  scrollTrigger: {
-    markers: true,
-    scroller: '#container',
-    trigger: "#color1",
-    pin: true,
-    start: 'top top',
-    end: 'bottom top',
-    scrub: true
-  }
-}); */
-
-manos.forEach(mano => {
-  if (mano.querySelector(".manoBrillo") != null) {
-    let manoBrillo = mano.querySelector(".manoBrillo");
-    ScrollTrigger.create({
-      //markers: true,
-      scroller: '#container',
-      trigger: mano,
-      start: '22% top+=60%',
-      onEnter: () => manoBrillo.classList.toggle('active'),
-      onLeaveBack: () => manoBrillo.classList.toggle('active')
-    });
-  }
-  
-  gsap.fromTo(mano, { y: 200 }, { y: -250,
-    scrollTrigger: {
-      //markers: true,
-      scroller: '#container',
-      trigger: mano,
-      start: 'top top+=95%',
-      end: 'top top-=50%',
-      scrub: 2
-      },
-    }
-  );
+  pinType: container.style.transform ? "transform" : "fixed"
 });
 
 titles.forEach( title => {
-  gsap.fromTo(title, { y: 100 }, { y: -30, 
+  gsap.fromTo(title, { y: 150 }, { y: -60, 
     scrollTrigger: {
-      //markers: true,
-      scroller: '#container',
+      scroller: container,
       trigger: title,
       start: 'top top+=90%',
-      end: 'top top+=30%',
+      end: 'top top+=40%',
       scrub: 1
       },
     }
@@ -81,8 +57,7 @@ titles.forEach( title => {
 paragraphs.forEach( ps => {
   gsap.from(ps, { y: 80, 
     scrollTrigger: {
-      //markers: true,
-      scroller: '#container',
+      scroller: container,
       trigger: ps,
       start: 'top top+=95%',
       end: 'top top+=65%',
@@ -90,6 +65,53 @@ paragraphs.forEach( ps => {
       },
     }
   );
+});
+
+manos.forEach(mano => {
+  if (mano.querySelector(".manoBrillo") != null) {
+    let manoBrillo = mano.querySelector(".manoBrillo");
+    ScrollTrigger.create({
+      scroller: container,
+      trigger: mano,
+      start: '22% top+=60%',
+      onEnter: () => manoBrillo.classList.toggle('active'),
+      onLeaveBack: () => manoBrillo.classList.toggle('active')
+    });
+  }
+  
+  gsap.fromTo(mano, { y: 200 }, { y: -250,
+    scrollTrigger: {
+      scroller: container,
+      trigger: mano,
+      start: 'top top+=95%',
+      end: 'top top-=50%',
+      scrub: 2
+      },
+    }
+  );
+});
+
+colorsTriggers.forEach((color, i) => {
+  gsap.fromTo(gradient, { background: `linear-gradient(to bottom, ${colors[i]}, ${colors[i + 1]})` }, {
+    background: `linear-gradient(to bottom, ${colors[i + 1]}, ${colors[i + 2]})`,
+    scrollTrigger: {
+      scroller: container,
+      trigger: color,
+      start: 'top top+=2px',
+      end: 'bottom top',
+      scrub: true
+    }
+  });
+});
+
+gsap.to(".gif", {
+  scale: 2, duration: 1.2, ease: "power3.inOut",
+  scrollTrigger: {
+    scroller: container,
+    trigger: ".gif",
+    start: 'top top+=100%',
+    toggleActions: "play none none reverse"
+  }
 });
 
 locoScroll.on("scroll", ScrollTrigger.update);
